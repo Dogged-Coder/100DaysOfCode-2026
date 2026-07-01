a6a86fa3628dc9037fd6fb77af20ec580f11bd4b
ManuDurga <you@example.com>
2026-07-01 18:12:38 +0530

    Question 2 Day 17

--- DSA/ManuDurga_590017881/Day_17/d17question2_technical.java
+++ DSA/ManuDurga_590017881/Day_17/d17question2_technical.java
@@ -1,53 +1,41 @@
 package Day_17;
 import java.util.*;
+
 class Node {
     int val;
     Node next;
-    Node prev;
 
     Node(int val) {
         this.val = val;
         this.next = null;
-        this.prev = null;
     }
 }
 
 public class d17question2_technical {
 
-    public static Node deleteLast(Node head) {
-        if (head == null) return null;
-
-        if (head.next == null) return null;
-
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
+    public static class Solution {
+        public boolean hasCycle(Node head) {
+            if (head == null) return false;
+            Node slow = head;
+            Node fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    return true;
+                }
+            }
+            return false;
         }
-
-        Node prevNode = temp.prev;
-        prevNode.next = null;  
-
-        return head;
-    }
-
-    public static void printList(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.val + " ");
-            temp = temp.next;
-        }
-        System.out.println();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter size of doubly linked list: ");
+        System.out.print("Enter size of list: ");
         int n = sc.nextInt();
-
+        System.out.println("Enter elements of list:");
         Node head = null, tail = null;
-
-        System.out.println("Enter " + n + " elements:");
         for (int i = 0; i < n; i++) {
             Node node = new Node(sc.nextInt());
             if (head == null) {
@@ -55,26 +43,23 @@ public class d17question2_technical {
                 tail = node;
             } else {
                 tail.next = node;
-                node.prev = tail;
                 tail = node;
             }
         }
 
-        head = deleteLast(head);
-
-        if (head == null) {
-            System.out.println("NULL");
-        } else {
-            Node temp = head;
-            while (temp != null) {
-                System.out.print(temp.val + " ");
-                temp = temp.next;
+        System.out.print("Enter pos (index to link tail, -1 for no cycle): ");
+        int pos = sc.nextInt();
+        if (pos != -1) {
+            Node cycleNode = head;
+            for (int i = 0; i < pos; i++) {
+                cycleNode = cycleNode.next;
             }
-            System.out.println();
+            tail.next = cycleNode;
         }
+
+        Solution sol = new Solution();
+        System.out.println(sol.hasCycle(head));
 
         sc.close();
     }
 }
-
-
